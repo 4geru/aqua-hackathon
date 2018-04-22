2a3b47bf861f59a331b559be876eaaee36dd6ad8
Rp7rf <westhouse51@gmail.com>
2018-04-22 13:20:24 +0900

    shop-ownerのindexを修正しました。

--- app/controllers/user_machines_controller.rb
+++ app/controllers/user_machines_controller.rb
@@ -3,13 +3,14 @@ class UserMachinesController < ApplicationController
   def index
     @shops_hash = aqua_api.shops.each_with_object({}) { |s, h| h[s["ANKSHOPID"]] = s }
     @machines = aqua_api.machines
+    @sales = UserMachine.where( "user_id = ?", current_user.id )
+    .map{|m| [ m[:shop_id], m[:machine_num]] }
   end
 
   def show
     machine = aqua_api.machine_detail(params[:shop_id], params[:machine_id])
     @shop = aqua_api.shop(params[:shop_id])
     @anual_sales = aqua_api.anual_sales(params[:shop_id], params[:machine_id])
-
     @user_machine = new_user_machine(machine)
   end
 
