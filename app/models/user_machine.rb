cc0f994c794cb709f3994cc67eeacf4f9c31372c
nakade.takuya <nakade.takuya@moneyforward.co.jp>
2018-04-22 12:55:19 +0900

    add shop

--- app/models/user_machine.rb
+++ app/models/user_machine.rb
@@ -1,2 +1,31 @@
 class UserMachine < ApplicationRecord
+  belongs_to :shop
+
+  class << self
+    def new_from_hash(user_id, machine)
+      new.tap do |m|
+        m.user_id = user_id
+        m.shop_id = machine["ANKSHOPID"].to_i
+        m.machine_num = machine["ANKMACHINENUM"].to_i
+        m.machine_short_name = machine["KNJMACHNAME"]
+        m.manufacture = machine["ANKMANUFACTURE"]
+        m.drum = machine["ANKDRUM"]
+        m.part_num = machine["MIXPARTNUM"]
+        m.machine_name = machine["KNJMACHINENAME"]
+        m.capacity = machine["NUMCAPACITY"]
+        m.process = machine["ANKPROCESS"]
+        m.payment = machine["ANKPAYMENT"]
+        m.machine_image = Base64.decode64(machine["BINMACHINEIMAGE"].split(",")[1])
+      end
+    end
+
+    def destroy_all_by_uqniue_keys(user_id, shop_id, machine_num)
+      where(
+        "user_id = ? and shop_id = ? and machine_num = ?",
+        user_id,
+        shop_id.to_i,
+        machine_num.to_i
+      ).destroy_all
+    end
+  end
 end
